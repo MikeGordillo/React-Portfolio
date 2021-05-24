01ecefba76b853aaf7b04c72dbb038d9113fe87e
Mike Gordillo <mikegordillotattoo@gmail.com>
2021-05-23 23:47:33 -0400

    Updates

---
+++ precache-manifest.9ff299ff4040e88cc8def3dc8f3c8452.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de967bcba81dd883d3e97bf4b520a0e",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "4273d7c1d3e1ec1251d7",
+    "url": "/React-Portfolio/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "a68d25fb7f7dff749ef1",
+    "url": "/React-Portfolio/static/js/2.72bcd546.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/React-Portfolio/static/js/2.72bcd546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4273d7c1d3e1ec1251d7",
+    "url": "/React-Portfolio/static/js/main.3dc7b5de.chunk.js"
+  },
+  {
+    "revision": "53cd5964469ae1da263b",
+    "url": "/React-Portfolio/static/js/runtime-main.01560ae1.js"
+  },
+  {
+    "revision": "6e55519d60121730c4397e14f646596b",
+    "url": "/React-Portfolio/static/media/mike.6e55519d.jpg"
+  },
+  {
+    "revision": "c9b92fc0f46357abf44ce231a0b505dc",
+    "url": "/React-Portfolio/static/media/resume.c9b92fc0.pdf"
+  }
+]);
