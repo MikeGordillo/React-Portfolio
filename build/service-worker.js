a0a147156e1bec3588ee9541fcd1e014d4471111
Mike Gordillo <mikegordillotattoo@gmail.com>
2021-05-24 00:31:08 -0400

    change pckg.JSON

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MikeGordillo/React-Portfolio/precache-manifest.35c868ca18c39333da89ae4459eac896.js"
+  "/React-Portfolio/precache-manifest.9ff299ff4040e88cc8def3dc8f3c8452.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MikeGordillo/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
